3a4e324f9ac53683f0b7a4824173333170e92249
Sam Oun Neang <somoun.neang@gmail.com>
2024-10-15 11:11:32 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NotificationChannelParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string InputMessageCase1 = "[BE][FE][Urgent] there is error";
+            string InputMessageCase2 = "[BE][QA][HAHA][Urgent] there is error";
+
+            Console.WriteLine($"\nInput: {InputMessageCase1}");
+            Console.WriteLine($"Receive channels: {ParseNotificationChannel(InputMessageCase1)}");
+
+            Console.WriteLine($"\nInput: {InputMessageCase2}");
+            Console.WriteLine($"Receive channels: {ParseNotificationChannel(InputMessageCase2)}");
+            
+        }
+        static string ParseNotificationChannel(string InputMessage)
+        {
+            string OutputMessage = string.Empty;
+            string ActiveChannel = "BE,FE,QA,Urgent";
+
+            string[] stringParts = InputMessage.Split(']');
+
+            for (int i = 0; i < stringParts.Length - 1; i++)
+            {
+                if (stringParts[i].StartsWith("["))
+                {
+                    string channel = stringParts[i].TrimStart('[');  // Remove the leading '['
+                    OutputMessage += ActiveChannel.Contains(channel) ? $"{channel}, " : "";
+                }
+            }
+
+            return OutputMessage.Remove(OutputMessage.LastIndexOf(","));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# NotificationChannelParser
+
+## Prerequisites
+- **SDK Version 8.0.403**  
+  [Download from Microsoft](https://dotnet.microsoft.com/en-us/download)
+
+## Clone the Repository
+  ```bash
+  git clone https://github.com/samounneang/NotificationChannelParser.git
+  ```
+
+## Run Project
+  ```bash
+  dotnet run
+  ```
