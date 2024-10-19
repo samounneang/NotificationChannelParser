8a017e9e33e773da171137e7073c613c7cad5714
Sam Oun Neang <somoun.neang@gmail.com>
2024-10-19 11:52:08 +0700

    refactor

--- Program.cs
+++ Program.cs
@@ -1,38 +1,35 @@
-﻿namespace NotificationChannelParser
+﻿using System;
+using System.Linq;
+
+namespace NotificationChannelParser
 {
     internal class Program
     {
+        private static readonly string[] ActiveChannels = { "BE", "FE", "QA", "Urgent" };
+
         static void Main(string[] args)
         {
-
-            string InputMessageCase1 = "[BE][FE][Urgent] there is error";
-            string InputMessageCase2 = "[BE][QA][HAHA][Urgent] there is error";
-
-            Console.WriteLine($"\nInput: {InputMessageCase1}");
-            Console.WriteLine($"Receive channels: {ParseNotificationChannel(InputMessageCase1)}");
-
-            Console.WriteLine($"\nInput: {InputMessageCase2}");
-            Console.WriteLine($"Receive channels: {ParseNotificationChannel(InputMessageCase2)}");
-            
-        }
-        static string ParseNotificationChannel(string InputMessage)
-        {
-            string OutputMessage = string.Empty;
-            string[] ActiveChannel = { "BE", "FE", "QA", "Urgent" };
-
-            string[] stringParts = InputMessage.Split(']');
-            
-
-            for (int i = 0; i < stringParts.Length - 1; i++)
+            var inputMessages = new[]
             {
-                if (stringParts[i].StartsWith("["))
-                {
-                    string channel = stringParts[i].TrimStart('[');
-                    OutputMessage += ActiveChannel.Contains(channel) ? $"{channel}, " : "";
-                }
-            }
+                "[BE][FE][Urgent] there is error",
+                "[BE][QA][HAHA][Urgent] there is error"
+            };
 
-            return OutputMessage.Remove(OutputMessage.LastIndexOf(","));
+            foreach (var message in inputMessages)
+            {
+                Console.WriteLine($"\nInput: {message}");
+                Console.WriteLine($"Receive channels: {ParseNotificationChannel(message)}");
+            }
+        }
+
+        static string ParseNotificationChannel(string inputMessage)
+        {
+            var channels = inputMessage
+                .Split(new[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(part => ActiveChannels.Contains(part))
+                .Distinct();
+
+            return string.Join(", ", channels);
         }
     }
 }
